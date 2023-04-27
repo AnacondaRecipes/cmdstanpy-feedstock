7a4dd10fb8a64a148cee91b77a8f49f8a077a391
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-27 15:57:27 +0300

    Debug tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -9,21 +9,21 @@ subprocess.run(['pip', 'check'])
 bernoulli_stan = os.path.join(cmdstan_path(), 'examples', 'bernoulli', 'bernoulli.stan')
 bernoulli_data = os.path.join(cmdstan_path(), 'examples', 'bernoulli', 'bernoulli.data.json')
 
-# # instantiate a model; compiles the Stan program by default
-# bernoulli_model = CmdStanModel(stan_file=bernoulli_stan)
-
-# # obtain a posterior sample from the model conditioned on the data
-# bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data)
-
-# # summarize the results (wraps CmdStan `bin/stansummary`):
-# bernoulli_fit.summary()
-
-# test with threading
 # instantiate a model; compiles the Stan program by default
-bernoulli_model = CmdStanModel(stan_file=bernoulli_stan, compile='force', cpp_options={'STAN_THREADS':True})
+bernoulli_model = CmdStanModel(stan_file=bernoulli_stan, compile=False)
 
 # obtain a posterior sample from the model conditioned on the data
-bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data, parallel_chains=2)
+bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data)
 
 # summarize the results (wraps CmdStan `bin/stansummary`):
 bernoulli_fit.summary()
+
+# # test with threading
+# # instantiate a model; compiles the Stan program by default
+# bernoulli_model = CmdStanModel(stan_file=bernoulli_stan, compile='force', cpp_options={'STAN_THREADS':True})
+
+# # obtain a posterior sample from the model conditioned on the data
+# bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data, parallel_chains=2)
+
+# # summarize the results (wraps CmdStan `bin/stansummary`):
+# bernoulli_fit.summary()

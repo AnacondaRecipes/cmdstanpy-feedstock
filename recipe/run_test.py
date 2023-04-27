10f4aab1df9fde537d84f50bb53a40fef6a4ff27
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-27 15:49:03 +0300

    Debug tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,18 +2,21 @@ import os
 import subprocess
 from cmdstanpy import cmdstan_path, CmdStanModel
 
+# run pip check
+subprocess.run(['pip', 'check'])
+
 # specify locations of Stan program file and data
 bernoulli_stan = os.path.join(cmdstan_path(), 'examples', 'bernoulli', 'bernoulli.stan')
 bernoulli_data = os.path.join(cmdstan_path(), 'examples', 'bernoulli', 'bernoulli.data.json')
 
-# instantiate a model; compiles the Stan program by default
-bernoulli_model = CmdStanModel(stan_file=bernoulli_stan)
+# # instantiate a model; compiles the Stan program by default
+# bernoulli_model = CmdStanModel(stan_file=bernoulli_stan)
 
-# obtain a posterior sample from the model conditioned on the data
-bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data)
+# # obtain a posterior sample from the model conditioned on the data
+# bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data)
 
-# summarize the results (wraps CmdStan `bin/stansummary`):
-bernoulli_fit.summary()
+# # summarize the results (wraps CmdStan `bin/stansummary`):
+# bernoulli_fit.summary()
 
 # test with threading
 # instantiate a model; compiles the Stan program by default
@@ -24,6 +27,3 @@ bernoulli_fit = bernoulli_model.sample(chains=4, data=bernoulli_data, parallel_c
 
 # summarize the results (wraps CmdStan `bin/stansummary`):
 bernoulli_fit.summary()
-
-# run pip check
-subprocess.run(['pip', 'check'])
